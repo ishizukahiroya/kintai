d4e5cd22fa24d7a8656560a5dcd828758057d852
ishizukahiroya <i.hiroya1215@gmail.com>
2022-04-20 00:07:45 +0900

    初回変更

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- logo.pngを自分で入れた画像の名前に変更 -->
+  <img alt="Vue logo" src="./assets/penguin.jpg" />
+  <!-- msg内に好きな言葉を入力 -->
+  <HelloWorld msg="I Love Coke" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
